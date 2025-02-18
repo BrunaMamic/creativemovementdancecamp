09093964ca7ad9515e3838dc5e46d52202e2e1ac
Bruna <brunamamic00@gmail.com>
2025-02-18 20:21:33 +0100

    gh pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: "/creativemovementdancecamp",
+};
 
 module.exports = withNextIntl(nextConfig);
