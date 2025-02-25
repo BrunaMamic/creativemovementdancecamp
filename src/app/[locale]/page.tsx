4f6a2b1dfe13b8c93832272056faf6bf937a1454
Bruna <brunamamic00@gmail.com>
2025-02-25 19:06:20 +0100

    mobile v few components

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,13 @@
-import { Accomodation } from "@/components/accomodation";
+// import { Accomodation } from "@/components/accomodation";
 import { Acordion } from "@/components/acordion";
-import { CampIdea } from "@/components/campIdea";
-import { Faq } from "@/components/faq";
+// import { CampIdea } from "@/components/campIdea";
+// import { Faq } from "@/components/faq";
 import { Hero2 } from "@/components/hero2";
 // import { InfiniteSlider } from "@/components/infiniteSlider";
-import { Lineup } from "@/components/lineup";
-import { Organizer } from "@/components/organizer";
-import { Schedule } from "@/components/schedule";
-import { TextSection } from "@/components/textSection";
+// import { Lineup } from "@/components/lineup";
+// import { Organizer } from "@/components/organizer";
+// import { Schedule } from "@/components/schedule";
+// import { TextSection } from "@/components/textSection";
 // import { TextSection } from "@/components/textSection";
 import { TextSectionV2 } from "@/components/textSectionV2";
 
@@ -16,14 +16,14 @@ export default function Home() {
     <>
       <Hero2 />
       <TextSectionV2 />
-      <Lineup />
-      <Schedule />
+      {/* <Lineup /> */}
+      {/* <Schedule /> */}
       <Acordion />
-      <Organizer />
+      {/* <Organizer />
       <TextSection />
       <CampIdea />
       <Accomodation />
-      <Faq />
+      <Faq /> */}
     </>
   );
 }
